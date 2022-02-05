c6123d1b3a1341e6859dc21bf24968818ec14310
Bruno Mortella <bmortella@gmail.com>
2022-02-05 12:00:11 -0300

    Passo 3

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,14 @@
 const list = document.getElementById("ul");
-const addInput = document.getElementById("add-input");
+const input = document.getElementById("add-input");
 const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", () => {
+  const li = `
+  <li class="list-group-item d-flex justify-content-between">
+      ${input.value} <button class="btn btn btn-danger">-</button>
+  </li>
+  `;
+  list.insertAdjacentHTML("beforeend", li);
+  input.value = "";
+  input.focus();
+});
